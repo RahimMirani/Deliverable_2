87dc92b6255f5ccde0ab81b0ca4f62c2dff9277d
abdul <abdul@DESKTOP-PEGG228>
2023-01-24 21:29:50 -0500

    Add project files.

---
+++ Deliverable_2/Program.cs
@@ -0,0 +1,56 @@
+﻿//Abdul Rahim Mirani
+//Deliverable 2
+//1/24/2023
+
+
+namespace Deliverable_2
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What grade do you expect in ISM 4300: ");
+
+            try
+            {
+                decimal grade = decimal.Parse(Console.ReadLine());
+
+                string letter;
+
+                if (grade >= 90)
+                {
+                    letter = "A";
+                    Console.WriteLine("Your letter grade is: " + letter);
+                }
+                else if (grade >= 80)
+                {
+                    letter = "B";
+                    Console.WriteLine("Your letter grade is: " + letter);
+                }
+                else if (grade >= 70)
+                {
+                    letter = "C";
+                    Console.WriteLine("Your letter grade is: " + letter);
+
+                }
+                else if (grade >= 60)
+                {
+                    letter = "D";
+                    Console.WriteLine("Your letter grade is: " + letter);
+                }
+                else 
+                {
+                    letter = "F";
+                    Console.WriteLine("Your letter grade is: " + letter);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a numeric value");
+            }
+        }
+    }
+}
